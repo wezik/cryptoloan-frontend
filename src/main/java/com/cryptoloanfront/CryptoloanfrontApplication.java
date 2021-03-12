4979f363dbb4b27cba9a907e9edc41792fdb1305
Dominik Kondrot <dominik.kondrot@gmail.com>
2021-03-12 01:34:00 +0100

    Base application

---
+++ src/main/java/com/cryptoloanfront/CryptoloanfrontApplication.java
@@ -0,0 +1,13 @@
+package com.cryptoloanfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoloanfrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoloanfrontApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cryptoloanfront/CryptoloanfrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cryptoloanfront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoloanfrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
