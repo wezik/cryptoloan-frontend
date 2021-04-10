c595e8ef64a0d37dc469214093440f1558c598d9
Dominik Kondrot <dominik.kondrot@gmail.com>
2021-04-10 23:28:56 +0200

    Refactor: LoanTypesView
    code cleanup
    improved readability by a lot

--- src/main/java/com/cryptoloanfront/views/loantypes/LoanTypesView.java
+++ src/main/java/com/cryptoloanfront/views/loantypes/LoanTypesView.java
@@ -1,16 +1,15 @@
 package com.cryptoloanfront.views.loantypes;
 
 import com.cryptoloanfront.domain.LoanType;
+import com.cryptoloanfront.factory.DivFactory;
+import com.cryptoloanfront.factory.FieldFactory;
 import com.cryptoloanfront.service.CryptoLoanService;
-import com.cryptoloanfront.views.BaseView;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.textfield.NumberField;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
@@ -23,23 +22,75 @@ import java.util.List;
 @Route("loantypes")
 @PageTitle("Crypto Loan")
 @CssImport("./styles/table-view.css")
-public class LoanTypesView extends BaseView {
+public class LoanTypesView extends Div {
 
+    private Button addButton, updateButton, clearButton, deleteButton;
+    private NumberField idField, deleteIdField, monthsField, interestField, punishmentField, minAmountField, maxAmountField;
+    private TextField nameField;
+    private Grid<LoanType> grid;
+    private final DivFactory divFactory;
+    private final FieldFactory fieldFactory;
+    private final List<LoanType> loanTypes;
     private final CryptoLoanService api;
-    private final Grid<LoanType> grid;
 
-    public LoanTypesView(@Autowired CryptoLoanService cryptoLoanService) {
-        api = cryptoLoanService;
-        List<LoanType> loanTypes = api.getLoanTypes();
+    public LoanTypesView(@Autowired CryptoLoanService cryptoLoanService,
+                     @Autowired DivFactory divFactory,
+                     @Autowired FieldFactory fieldFactory) {
+        this.divFactory = divFactory;
+        this.fieldFactory = fieldFactory;
+        this.api = cryptoLoanService;
+        this.loanTypes = api.getLoanTypes();
+        Div addDiv = createAddDiv();
+        Div deleteDiv = createDeleteDiv();
+        Div gridDiv = createGridDiv();
+        createInteractions();
+        Div main = divFactory.createCompleteDiv("Loan Types",addDiv,deleteDiv,gridDiv);
+        add(main);
+    }
 
-        Div horizontalDiv = new Div();
-        horizontalDiv.setId("hDiv");
+    private Div createAddDiv() {
+        Div addDiv = divFactory.createDivWithId("addDiv");
+        idField = fieldFactory.createIdNumberField();
+        nameField = fieldFactory.createTextField("Name");
+        monthsField = fieldFactory.createIdNumberField("Months","12");
+        interestField = fieldFactory.createIdNumberField("Interest (%)","12.3");
+        punishmentField = fieldFactory.createIdNumberField("Punishment (%)","12.3");
+        minAmountField = fieldFactory.createNumberField("Min. Amount","1234.56");
+        maxAmountField = fieldFactory.createNumberField("Max. Amount", "1234.56");
+        List<Component> addMenuFieldsList = List.of(
+                idField,
+                nameField,
+                monthsField,
+                interestField,
+                punishmentField,
+                minAmountField,
+                maxAmountField
+        );
+        addButton = fieldFactory.createAddButton();
+        updateButton = fieldFactory.createUpdateButton();
+        clearButton = fieldFactory.createClearButton();
+        addMenuFieldsList.forEach(addDiv::add);
+        addDiv.add(addButton,updateButton,clearButton);
+        return addDiv;
+    }
 
-        Div main = getMain();
+    private Div createDeleteDiv() {
+        Div deleteDiv = divFactory.createDivWithId("deleteDiv");
+        deleteIdField = fieldFactory.createDeleteIdNumberField();
+        deleteButton = fieldFactory.createDeleteButton();
+        deleteDiv.add(deleteIdField,deleteButton);
+        return deleteDiv;
+    }
 
-        main.add(new H1("Loan Types"));
-        main.add(horizontalDiv);
-        grid = new Grid<>();
+    private Div createGridDiv() {
+        Div div = divFactory.createDivWithId("grid-div");
+        grid = createGrid();
+        div.add(grid);
+        return div;
+    }
+
+    private Grid<LoanType> createGrid() {
+        Grid<LoanType> grid = new Grid<>();
         grid.setItems(loanTypes);
         grid.addColumn(LoanType::getId).setHeader("ID");
         grid.addColumn(LoanType::getName).setHeader("Name");
@@ -48,116 +99,81 @@ public class LoanTypesView extends BaseView {
         grid.addColumn(LoanType::getPunishment).setHeader("Punishment (%)");
         grid.addColumn(LoanType::getMinAmount).setHeader("Min. Amount");
         grid.addColumn(LoanType::getMaxAmount).setHeader("Max. Amount");
-        main.add(grid);
-
-        Div addLoanTypeDiv = createInstallmentDiv();
-
-        Div deleteLoanTypeDiv = createRemoveInstallmentDiv();
-
-        horizontalDiv.add(addLoanTypeDiv,deleteLoanTypeDiv);
-
-        Div annotation = new Div();
-        annotation.setText("Fields annotated with (*) are not required or even recommended when adding an entity.");
-        Div annotation2 = new Div();
-        annotation2.setText("Also select an entity to edit it faster.");
-        main.add(annotation,annotation2);
-
-        add(main);
+        return grid;
     }
 
-    private Div createInstallmentDiv() {
-        Div addLoanTypeDiv = new Div();
-        addLoanTypeDiv.setId("addDiv");
-        NumberField id = new NumberField("ID *");
-        id.setId("idInput");
-        id.setPlaceholder("0");
-        TextField name = new TextField("Name");
-        name.setPlaceholder("Name");
-        NumberField timePeriod = new NumberField("Months");
-        timePeriod.setId("idInput");
-        timePeriod.setPlaceholder("12");
-        NumberField interest = new NumberField("Interest (%)");
-        interest.setId("idInput");
-        interest.setPlaceholder("12.3");
-        NumberField punishment = new NumberField("Punishment (%)");
-        punishment.setId("idInput");
-        punishment.setPlaceholder("12.3");
-        NumberField minAmount = new NumberField("Min. Amount");
-        minAmount.setPlaceholder("1234.56");
-        NumberField maxAmount = new NumberField("Max. Amount");
-        maxAmount.setPlaceholder("1234.56");
-        List<Component> loanTypeData = List.of(id,name,timePeriod,interest,punishment,minAmount,maxAmount);
-        for (int i=1; i<loanTypeData.size(); i++) {
-            loanTypeData.get(i).setId("inputField");
-        }
+    private void createInteractions() {
+        createGridSelectionListener();
+        createClickListenerForAddButton();
+        createClickListenerForUpdateButton();
+        createClickListenerForClearButton();
+        createClickListenerForDeleteButton();
+    }
 
+    private void createGridSelectionListener() {
         grid.addSelectionListener(e -> {
             if (e.getFirstSelectedItem().isPresent()) {
                 LoanType loanType = e.getFirstSelectedItem().get();
-                id.setValue(loanType.getId().doubleValue());
-                name.setValue(loanType.getName());
-                timePeriod.setValue(loanType.getTimePeriod().doubleValue());
-                interest.setValue(loanType.getInterest());
-                punishment.setValue(loanType.getPunishment());
-                minAmount.setValue(Double.parseDouble(loanType.getMinAmount()));
-                maxAmount.setValue(Double.parseDouble(loanType.getMaxAmount()));
+                idField.setValue(loanType.getId().doubleValue());
+                deleteIdField.setValue(loanType.getId().doubleValue());
+                nameField.setValue(loanType.getName());
+                monthsField.setValue(loanType.getTimePeriod().doubleValue());
+                interestField.setValue(loanType.getInterest());
+                punishmentField.setValue(loanType.getPunishment());
+                minAmountField.setValue(Double.parseDouble(loanType.getMinAmount()));
+                maxAmountField.setValue(Double.parseDouble(loanType.getMaxAmount()));
             }
         });
-
-        Button add = new Button("Add");
-        add.addClickListener(e -> {
-            api.saveLoanType(new LoanType(null,
-                    name.getValue(),
-                    timePeriod.getValue().intValue(),
-                    interest.getValue(),
-                    punishment.getValue(),
-                    BigDecimal.valueOf(minAmount.getValue()).toPlainString(),
-                    BigDecimal.valueOf(maxAmount.getValue()).toPlainString()));
-            UI.getCurrent().getPage().reload();
-            Notification.show("Loan type "+name.getValue()+" Added");
-        });
-        Button update = new Button("Update");
-        update.addClickListener(e -> {
-            api.updateLoanType(new LoanType(id.getValue().longValue(),
-                    name.getValue(),
-                    timePeriod.getValue().intValue(),
-                    interest.getValue(),
-                    punishment.getValue(),
-                    BigDecimal.valueOf(minAmount.getValue()).toPlainString(),
-                    BigDecimal.valueOf(maxAmount.getValue()).toPlainString()));
-            UI.getCurrent().getPage().reload();
-            Notification.show("Loan type "+id.getValue().intValue()+" Updated");
-        });
-        Button clear = new Button("Clear");
-        clear.addClickListener(e -> {
-            id.clear();
-            name.clear();
-            timePeriod.clear();
-            interest.clear();
-            punishment.clear();
-            minAmount.clear();
-            maxAmount.clear();
-        });
-        loanTypeData.forEach(addLoanTypeDiv::add);
-        addLoanTypeDiv.add(add,update,clear);
-        return addLoanTypeDiv;
     }
 
-    private Div createRemoveInstallmentDiv() {
-        Div deleteInstallmentDiv = new Div();
-        deleteInstallmentDiv.setId("deleteDiv");
-        NumberField deleteId = new NumberField("ID");
-        deleteId.setId("idInput");
-        deleteId.setPlaceholder("0");
-        Button deleteButton = new Button("Delete");
-        deleteButton.addClickListener(e -> {
-            api.deleteLoanType(deleteId.getValue().intValue());
+    private void createClickListenerForAddButton() {
+        addButton.addClickListener(e -> {
+            LoanType loanType = new LoanType(null,
+                    nameField.getValue(),
+                    monthsField.getValue().intValue(),
+                    interestField.getValue(),
+                    punishmentField.getValue(),
+                    BigDecimal.valueOf(minAmountField.getValue()).toPlainString(),
+                    BigDecimal.valueOf(maxAmountField.getValue()).toPlainString()
+            );
+            api.saveLoanType(loanType);
             UI.getCurrent().getPage().reload();
-            Notification.show("Loan type " + deleteId.getValue().intValue() + " Deleted");
         });
-        deleteButton.setId("deleteButton");
-        deleteInstallmentDiv.add(deleteId,deleteButton);
-        return deleteInstallmentDiv;
+    }
+
+    private void createClickListenerForUpdateButton() {
+        updateButton.addClickListener(e -> {
+            LoanType loanType = new LoanType(idField.getValue().longValue(),
+                    nameField.getValue(),
+                    monthsField.getValue().intValue(),
+                    interestField.getValue(),
+                    punishmentField.getValue(),
+                    BigDecimal.valueOf(minAmountField.getValue()).toPlainString(),
+                    BigDecimal.valueOf(maxAmountField.getValue()).toPlainString()
+            );
+            api.updateLoanType(loanType);
+            UI.getCurrent().getPage().reload();
+        });
+    }
+
+    private void createClickListenerForClearButton() {
+        clearButton.addClickListener(e -> {
+            idField.clear();
+            deleteIdField.clear();
+            nameField.clear();
+            monthsField.clear();
+            interestField.clear();
+            punishmentField.clear();
+            minAmountField.clear();
+            maxAmountField.clear();
+        });
+    }
+
+    private void createClickListenerForDeleteButton() {
+        deleteButton.addClickListener(e -> {
+            api.deleteLoanType(deleteIdField.getValue().intValue());
+            UI.getCurrent().getPage().reload();
+        });
     }
 
 }
